6764ed2e152537da205cb615214161304ba220a2
Dan <dan@dan-VirtualBox.(none)>
2012-11-28 10:22:11 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BidmcInitech::Application.config.secret_token = '203826ecb017589a34af0bb5c3e8ae92e7e8f134915a8a3ad9be0cf8580d1066a9f3216bac958a7410c5c543c8e1a3c2a49fd5784401f76f5dee2357bb384924'
