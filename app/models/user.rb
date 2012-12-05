dd0fb3470c5544e39b30ce01f69cc7752f35c741
tkaruzis <cheesecurler@gmail.com>
2012-12-05 08:46:24 -0500

    user/new validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :password, :email
 
+  validates_presence_of :first_name, :last_name, :password, :email
+  validates_format_of :email, with: /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
+  validates_format_of :password, with: /^((?=.*\d)(?=.*[a-z]).{6,20})/i, message: "must be more than 5 characters, and contain letters and numbers"
+  validates_uniqueness_of :email
+
   #to add: internal mailing address, telephone, pager, fax
 
   def proper_name #returns lastname, firstname
